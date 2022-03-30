e1d495906f504441cf5b85dfe368a4449141df29
den413 <92449901+den413@users.noreply.github.com>
2022-03-30 21:56:34 +0900

    componet type apply

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,8 @@ import axios from "axios";
 import { useState } from "react";
 import "./styles.css";
 import { Todo } from "./Todo";
-
-type TodoType = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-};
+import { Text } from "./Text";
+import { TodoType } from "./types/todo";
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -23,9 +18,11 @@ export default function App() {
 
   return (
     <div className="App">
+      <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
         <Todo
+          key={todo.id}
           title={todo.title}
           userId={todo.userId}
           completed={todo.completed}
